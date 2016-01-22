f9b60187a20084ded7ceb76aa2084a3388498bfe
ariarijp <takuya.arita@gmail.com>
2016-01-22 17:33:49 +0900

    Initial implementation

---
+++ takoashi.go
@@ -0,0 +1,66 @@
+package takoashi
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"sync"
+)
+
+func getFilename(u string) string {
+	_u, err := url.Parse(u)
+	if err != nil {
+		log.Fatal(fmt.Sprint("Failed parse as URL", u, "-", err))
+	}
+
+	return path.Base(_u.Path)
+}
+
+func createFile(fn string, res *http.Response) {
+	out, err := os.Create(fn)
+	if err != nil {
+		log.Fatal(fmt.Sprint("Error while creating", fn, "-", err))
+	}
+	defer out.Close()
+
+	n, err := io.Copy(out, res.Body)
+	if err != nil {
+		log.Fatal(fmt.Sprint("Error while downloading", fn, "-", err))
+	}
+
+	fmt.Println(res.Request.URL, res.Status, n, "bytes downloaded.")
+}
+
+func download(wait *sync.WaitGroup, u string) {
+	fn := getFilename(u)
+
+	res, err := http.Get(u)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	createFile(fn, res)
+
+	wait.Done()
+}
+
+func main() {
+	if len(os.Args) == 1 {
+		log.Fatal("Missing required arguments")
+	}
+
+	urls := os.Args[1:]
+	wait := new(sync.WaitGroup)
+
+	for _, u := range urls {
+		wait.Add(1)
+		go download(wait, u)
+	}
+
+	wait.Wait()
+}
